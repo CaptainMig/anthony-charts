4a7f670f1f3d29516eaa1c7782340aeeb133ff3e
CaptainMig <miglanthony@gmail.com>
2026-03-18 11:02:00 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
+import fs from 'fs'
+import path from 'path'
+
 export default function Home() {
-  return (
-    <main style={{margin:0,padding:0}}>
-      <iframe 
-        src="/index.html" 
-        style={{width:'100vw',height:'100vh',border:'none',display:'block'}}
-      />
-    </main>
-  )
+  const html = fs.readFileSync(path.join(process.cwd(), 'public/index.html'), 'utf-8')
+  return <div dangerouslySetInnerHTML={{ __html: html }} />
 }
