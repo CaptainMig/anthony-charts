8a94643772dab210cdd4352771078caadabc8848
Anthony M. <miglanthony@gmail.com>
2026-03-23 11:41:38 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from 'next/navigation'
+
 export default function Home() {
-  return (
-    <iframe
-      src="/index.html"
-      style={{ width: '100vw', height: '100vh', border: 'none' }}
-    />
-  )
+  redirect('/index.html')
 }
